e459993454e569ca172fde6282b3b8bf58da2b00
Abhradip Paul <111123303+abhradippaul@users.noreply.github.com>
2024-08-18 21:19:04 +0530

    Drizzle setup completed with neon postgres database

---
+++ src/database/schema.ts
@@ -0,0 +1,14 @@
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull(),
+  image: text("image").notNull(),
+  createdAt: timestamp("createdAt").defaultNow().notNull(),
+});
